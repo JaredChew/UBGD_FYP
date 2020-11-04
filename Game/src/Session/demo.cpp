713df53959a8735b46fb009a61d96b5cfd173e88
hosiang <hosiang79@hotmail.com>
2020-11-04 13:33:31 +0800

    Merge: -demo

--- Game/src/Session/demo.cpp
+++ Game/src/Session/demo.cpp
@@ -6,14 +6,23 @@
 #include "Modules BackEnd/camera.h"
 #include "Manipulators/renderer.h"
 
+#include "Manipulators/system.h"
+#include "Manipulators/shader.h"
+
+#include "../global.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 Demo::Demo() {
 
-	texture = new Texture("../Media/back.bmp");
+	glEnable(GL_TEXTURE_2D);
+	texture = new Texture("../Media/TreasureNormal.png");
 	mesh = new Mesh(Mesh::DefaultGeometry::SQUARE);
 	transform = new Transform();
 
 	Renderer::getInstance()->useMesh(mesh);
-	Renderer::getInstance()->useTexture(texture->getTextureID());
+	//Renderer::getInstance()->useTexture(texture->getTextureID());
 
 }
 
@@ -27,7 +36,56 @@ void Demo::update() {
 
 void Demo::render() {
 
+	Renderer::getInstance()->useTexture(texture->getTextureID());
 	Renderer::getInstance()->render(transform->getModelMatrix());
 	Renderer::getInstance()->renderToScreen();
+	/*
+	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
 
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderTexture, 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
+
+
+	if (System::checkIsFramebufferReady()) {
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		Shader::defaultDraw(glm::scale(glm::mat4(1.0f), glm::vec3(0.5, 0.5, 0.5)));
+
+		glActiveTexture(0);
+		glBindTexture(GL_TEXTURE_2D, texture->getTextureID());
+
+		glBindVertexArray(mesh->getVAO());
+		glDrawElements(GL_TRIANGLES, mesh->getIndicesSize(), GL_UNSIGNED_INT, 0);
+
+	}
+
+	
+	static bool oneTiem = false;
+	if (!oneTiem)
+	{
+		//System::saveTextureToImage("../Media/abc01.png", texture->getTextureID(), 512, 512);
+		oneTiem = true;
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	if (System::checkIsFramebufferReady()) {
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//glBindTexture(GL_TEXTURE_2D, frontBuffer);
+
+		glm::mat4 mvpMatrix = Renderer::getInstance()->camera->getProjectionMatrix() * Renderer::getInstance()->camera->getViewMatrix() * transform->getModelMatrix();
+		Shader::defaultDraw(glm::mat4(1.0f));
+		
+		glActiveTexture(0);
+		glBindTexture(GL_TEXTURE_2D, renderTexture);
+
+		glBindVertexArray(mesh->getVAO());
+		glDrawElements(GL_TRIANGLES, mesh->getIndicesSize(), GL_UNSIGNED_INT, 0);
+
+	}
+	*/
+	
+	
 }
